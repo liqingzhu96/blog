4d8cff2968e9cc755399c2b7864ab1fba4a80752
liqingzhu96 <463109762@qq.com>
2015-09-22 20:32:56 +0800

    Admin

--- blog/server/action.js
+++ blog/server/action.js
@@ -26,20 +26,29 @@ articlesModel.find({},"_id title type time content", {sort: {'_id':-1},limit:6},
 		});
 /*end*/
 function index(req,res){
-articlesModel.find({},"_id title type time content", {sort: {'_id':-1},limit:10},function(err,data){
+var page=url.parse(req.url,true).query['page']||1;
+articlesModel.count(function(err,count){
+	if(err){console.log('数量查询出错！')}
+	var articlesCount=parseInt(count/10)+1; 
+	articlesCount<page?page=articlesCount:page=page;
+	console.log("articlesCount:"+articlesCount);
+	articlesModel.find({},"_id title type time content", {sort: {'_id':-1},limit:page*10,skip:(page-1)*10},function(err,data){
 			if(err){
 				console.log(err);
 				res.writeHead(502,{"content-type":"text/html"});
 				res,write("502 服务器出错！");
 				res.end();
 			}else{
+				articlesData=data;
 				res.writeHead(200,{"content-type":"text/html"});
-				var html=jade.renderFile('../jade/index.jade',{'data':data});
-				console.log("数据库中查询了数据："+data);
+				var html=jade.renderFile('../jade/index.jade',{'DATA':DATA,'data':articlesData,'count':articlesCount,'page':page});
+				console.log("数据库中的数据数量："+count);
 				res.write(html);
 				res.end();
 			}
 		});
+	});
+
 }
 function blog(req,res){
 	var id=url.parse(req.url,true).query['_id'];
