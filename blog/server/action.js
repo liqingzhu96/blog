4bc396db244f5a09bce318ebeba850670a1fe0c7
liqingzhu96 <463109762@qq.com>
2015-09-25 21:53:36 +0800

    ji ben wan cheng

--- blog/server/action.js
+++ blog/server/action.js
@@ -12,7 +12,8 @@ var articlesSchema=new mongoose.Schema({
 	"title":String,
 	"type":String,
 	"content":String,
-	"time":String
+	"time":String,
+	"date":String
 });
 var loginsSchema=new mongoose.Schema({
 	"username":String,
@@ -26,15 +27,22 @@ console.log("数据调用1次！");
 
 /*缓存*/
 var DATA;
-articlesModel.find({},"_id title type time content", {sort: {'_id':-1},limit:6},function(err,data){
-			if(err){
-				console.log("缓存ERROR:"+err);
-			}else{
-				DATA=data;
-			}
-		});
+function huancun(){
+	articlesModel.find({},"_id title type time content", {sort: {'date':-1},limit:6},function(err,data){
+		if(err){
+			console.log("缓存ERROR:"+err);
+		}else{
+			DATA=data;
+		}
+	});
+}
+huancun();
 /*end*/
-
+function send404(res){
+	res.writeHead(404,{"content-type":"text/html"});
+	res.write("404 Not Found!");
+	res.end();
+}
 /*博客全览*/
 function index(req,res){
 var page=url.parse(req.url,true).query['page']||1;
@@ -43,7 +51,7 @@ articlesModel.count(function(err,count){
 	var articlesCount=parseInt(count/10)+1; 
 	articlesCount<page?page=articlesCount:page=page;
 	console.log("articlesCount:"+articlesCount);
-	articlesModel.find({},"_id title type time content", {sort: {'_id':-1},limit:page*10,skip:(page-1)*10},function(err,data){
+	articlesModel.find({},"_id title type time content", {sort: {'date':-1},limit:page*10,skip:(page-1)*10},function(err,data){
 			if(err){
 				console.log(err);
 				res.writeHead(502,{"content-type":"text/html"});
@@ -71,17 +79,23 @@ function blog(req,res){
 				res,write("502 服务器出错！");
 				res.end();
 			}else{
-				res.writeHead(200,{"content-type":"text/html"});
-				var html=jade.renderFile('../jade/blog.jade',{'data':data,'DATA':DATA});
 				console.log("数据库中查询了数据："+data);
-				res.write(html);
-				res.end();
+				if(data)
+				{	res.writeHead(200,{"content-type":"text/html"});
+					var html=jade.renderFile('../jade/blog.jade',{'data':data,'DATA':DATA});
+					res.write(html);
+					res.end();
+				}
+				else{
+					send404(res);
+				}
 			}
 		});
 }
 /*登陆*/
 
 function admin(req,res){
+	res.writeHead(200,{"content-type":"text/html"});
 	var html=jade.renderFile('../jade/admin.jade');
 	res.write(html);
 	res.end();
@@ -100,19 +114,104 @@ function login(req,res){
 			}
 			if(data.length==0){
 				console.log("登陆失败！");
-				admin(req,res)
+				res.write("<script>window.location='/admin'</script>");
 			}
 			else{
 				console.log("登陆成功！");
 				res.setHeader('Set-Cookie',"username=liqingzhu;Max-Age=3600");
-				index(req,res);
+				res.write("<script>window.location='/index'</script>");
 			}
 		})
 	});
 
 }
+/**/
+/*发博客*/
+function xiugai(req,res){
+	res.writeHead(200,{"content-type":"text/html"});
+		var _id=url.parse(req.url,true).query['_id'];
+		articlesModel.findById(_id,function(err,data){
+			var html=jade.renderFile('../jade/xiugai.jade',{'data':data});
+			res.write(html);
+			res.end();
+		})
+}
+function fabiao(req,res){
+	res.writeHead(200,{"content-type":"text/html"});
+	var html=jade.renderFile('../jade/fabiao.jade');
+	res.write(html);
+	res.end();
+}
+function add(req,res){
+	var post="";
+	req.on('data',function(data){
+		post+=data;
+	});
+	req.on('end',function(){
+		post=querystring.parse(post);
+		post['time']=new Date().toLocaleString();
+		post['date']=new Date().getTime();
+		post['content']=post['content'].replace(/\r\n/g,'<br>');
+		var fabiao=new articlesModel(post);
+		fabiao.save(function(err){
+			if(err){
+				console.log("update ERROR:"+err);
+				res.write("<script>alert('操作失败，请重试！');window.history.back(-1);</script>");
+				res.end();
+			}else{
+				huancun();
+				res.write("<script>alert('操作成功！');window.location='/index'</script>");
+			}
+		});
 
+	});
+}
+function update(req,res){
+	var post="";
+	var _id=url.parse(req.url,true).query['_id'];
+	req.on('data',function(data){
+		post+=data;
+	});
+	req.on('end',function(){
+		post=querystring.parse(post);
+		post['time']=new Date().toLocaleString();
+		post['date']=new Date().getTime();
+		post['content']=post['content'].replace(/\r\n/g,'<br>');
+		var update={$set:post};
+		articlesModel.update({'_id':_id},update,function(err){
+			if(err){
+				console.log("update ERROR:"+err);
+				res.write("<script>alert('修改失败，请重试！');window.history.back(-1);</script>");
+				res.end();
+			}else{
+				huancun();
+				res.write("<script>alert('修改成功！');window.location='/index'</script>");
+			}
+		});
+	});
+}
+/**/
+/*删除！*/
+function remove(req,res){
+	var _id=url.parse(req.url,true).query['_id'];
+	articlesModel.remove({'_id':_id},function(err){
+		if(err){
+			console.log("update ERROR:"+err);
+			res.write("<script>alert('删除失败，请重试！');window.history.back(-1);</script>");
+			res.end();
+		}else{
+			huancun();
+			res.write("<script>alert('删除成功！');window.location='/index'</script>");
+		}
+	});
+}
+/**/
 exports.index=index;
 exports.blog=blog;
 exports.admin=admin;
 exports.login=login;
+exports.fabiao=fabiao;
+exports.xiugai=xiugai;
+exports.update=update;
+exports.add=add;
+exports.remove=remove;
--- blog/server/mongo.js
+++ blog/server/mongo.js
@@ -11,10 +11,11 @@ var articlesSchema=new mongoose.Schema({
 });
  var articlesModel=db.model('articles',articlesSchema);
 	var articlesEntity=new articlesModel({
-		"title":"哈哈哈10",
+		"_id":"55fe8cf6ece7f6400ba04aac",
+		"title":"xixixixiixixixi",
 		"type":"心情笔记",
 		"content":"FuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuckFuck",
-		"time":"2015/9/20"
+		"time":"2015/9/26"
 	});
 	articlesEntity.save(function(err){
 		if(err){
